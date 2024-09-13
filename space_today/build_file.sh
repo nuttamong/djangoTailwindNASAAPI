631094350a1b2d4c38827936a059f630363fba4b
nuttamong <nuttamong@kkumail.com>
2024-09-13 21:54:16 +0700

    complete

--- space_today/build_file.sh
+++ space_today/build_file.sh
@@ -11,6 +11,7 @@ pip install -r requirements.txt
 npm i rimraf
 npm i --save-dev cross-env
 npm i tailwindcss
+npm i --save-dev postcss-import
 
 python3 manage.py tailwind build
 python3 manage.py collectstatic --noinput
