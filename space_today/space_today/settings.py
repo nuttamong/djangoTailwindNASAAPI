89b73989a713635f6f4a59592f447358522852e9
nuttamong <nuttamong@kkumail.com>
2024-09-13 19:51:07 +0700

    complete

--- space_today/space_today/settings.py
+++ space_today/space_today/settings.py
@@ -53,7 +53,7 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-# NPM_BIN_PATH = "D:/Program Files/nodejs/npm.cmd"#"/usr/local/bin/npm"
+NPM_BIN_PATH = "/usr/local/bin/npm"#"D:/Program Files/nodejs/npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
